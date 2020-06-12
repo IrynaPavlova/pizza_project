c9cea02ffbf0fe149988e4e0ff5153e8a3a2cecd
Pavlova <irochkapavlova85@gmail.com>
2020-06-12 11:31:22 +0300

    fix getToken

--- src/helpers/getToken.js
+++ src/helpers/getToken.js
@@ -1,6 +1,7 @@
 const getToken = request =>
   request.body.token ||
   request.query.token ||
-  request.headers["x-access-token"];
+  request.headers["x-access-token"] ||
+  request.get("Authorization").replace("Bearer ", "");
 
 module.exports = getToken;
