91f288eeef4a51a83ce8aac816c9f8109a580ea1
Pavlova <irochkapavlova85@gmail.com>
2020-06-12 11:21:14 +0300

    edit

--- src/routes/auth/controllers/authLogout.js
+++ src/routes/auth/controllers/authLogout.js
@@ -4,10 +4,8 @@ const authLogout = async (request, response) => {
   try {
     let token = getToken(request);
     if (token) {
-      token = null;
-      response
-        .status(200)
-        .send({ status: "success", text: "Logout success", token: token });
+      token = "";
+      response.status(200).send({ status: "success", token: token });
     } else
       response.status(403).send({
         status: "error",
