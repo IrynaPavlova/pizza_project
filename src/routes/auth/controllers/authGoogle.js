241870c7884f0e79ca580bed241a47e0d26b3ccc
Pavlova <irochkapavlova85@gmail.com>
2020-06-14 17:38:23 +0300

    edits

--- src/routes/auth/controllers/authGoogle.js
+++ src/routes/auth/controllers/authGoogle.js
@@ -5,12 +5,15 @@ const authGoogle = async (req, res) => {
     const user = req.user;
     const userName = user.username;
     const userId = user._id;
+    const email = user._id;
     const payload = { userId };
     const token = generateToken(payload);
 
     return res
       .status(201)
-      .redirect(`http://localhost:3000/auth/?token=${token}&name=${userName}`);
+      .redirect(
+        `http://localhost:3000/auth/?token=${token}&name=${userName}&email=${email}`
+      );
   } catch (err) {
     res.status(404).json({
       status: "error",
