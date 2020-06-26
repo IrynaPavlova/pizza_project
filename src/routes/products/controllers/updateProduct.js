c9d2eec4e01e9401516e8161d4049b958e6279a6
Pavlova <irochkapavlova85@gmail.com>
2020-06-26 17:11:49 +0300

    edits

--- src/routes/products/controllers/updateProduct.js
+++ src/routes/products/controllers/updateProduct.js
@@ -1,5 +1,24 @@
 const Product = require("../productSchema");
 const getToken = require("../../../helpers/getToken");
+const Joi = require("joi");
+
+const categories = ["pizza", "drinks", "sides", "desserts"];
+const subcategories = ["classic", "premium", "branded", ""];
+
+const validation = Joi.object().keys({
+  sku: Joi.number(),
+  name: Joi.object().required(),
+  description: Joi.string(),
+  price: Joi.object().required(),
+  currency: Joi.string(),
+  categories: Joi.string()
+    .valid(...categories)
+    .required(),
+  subcategory: Joi.string().valid(...subcategories),
+  likes: Joi.number(),
+  images: Joi.string().required(),
+  ingredients: Joi.array()
+});
 
 const updateProduct = async (request, response) => {
   try {
@@ -10,16 +29,18 @@ const updateProduct = async (request, response) => {
     //     message: "No token provided"
     //   });
     // }
-    const product = request.body;
+    const product = Joi.validate(request.body, validation);
+    if (product.error) {
+      return response.status(400).json(product.error.details[0].message);
+    }
     const id = request.params.id;
 
     const updatedProduct = await Product.findOneAndUpdate(
       { _id: id },
       product,
-      {
-        new: true
-      }
+      { new: true }
     );
+
     response.status(201).json({
       status: "success",
       product: updatedProduct
